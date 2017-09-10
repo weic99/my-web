9b08cd0e8f602e78e5f6716f70b767931f395d0c
Wei Chen <tengwei1@gmail.com>
2017-09-10 17:32:53 -0400

    imported mdbuttonmodule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MdToolbarModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    MdToolbarModule
+    MdToolbarModule,
+    MdButtonModule
   ],
   exports: [
-    MdToolbarModule
+    MdToolbarModule,
+    MdButtonModule
   ]
 })
 export class SharedModule {}
