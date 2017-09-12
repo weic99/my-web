ec7d2bdeff8617ddcf91d5000e35833104a3840d
Wei Chen <tengwei1@gmail.com>
2017-09-12 15:25:44 -0400

    added button for linkedin and github

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  github = 'https//github.com/weic99';
+  github = 'https://github.com/weic99';
+  linkedin = 'https://www.linkedin.com/in/wei-chen-b44a8012b/';
 
   constructor() { }
 
